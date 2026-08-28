84051d57b1d5c0f26877d3a76a7c50b4c798df91
Eng. Mohammed N. Al-Areqi <279513615+mnabil-alareqi@users.noreply.github.com>
2026-08-28 15:02:08 +0300

    Update q8.sql

--- lecture10/q8.sql
+++ lecture10/q8.sql
@@ -1,4 +1,4 @@
--- عرض ارقام واسماء ورواتب من جدول الموظفين واسماء الادارات من جدول الادارات والفئات التي تنتمي لها رواتبهم من جدول الفئات
+--عرض ارقام واسماء ورواتب من جدول الموظفين واسماء الادارات من جدول الادارات والفئات التي تنتمي لها رواتبهم من جدول الفئات
 select e.empno,e.ename,e.sal,d.dname,s.grade
 from emp e,dept d,salgrade s
 where e.deptno=d.deptno and e.sal between s.losal and s.hisal;
