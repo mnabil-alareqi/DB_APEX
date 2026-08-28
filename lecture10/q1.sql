d287eec03e76ac0bf9186cd6b6fde4e5a2f18992
Eng. Mohammed N. Al-Areqi <279513615+mnabil-alareqi@users.noreply.github.com>
2026-08-28 14:56:54 +0300

    Update q1.sql

--- lecture10/q1.sql
+++ lecture10/q1.sql
@@ -1,4 +1,4 @@
--- عرض رقم واسم الموظف ورقم الادارة من جدول الموظفين ورقم الادارة وموفعها من جدول الادارات
+-- عرض رقم واسم الموظف ورقم الادارة من جدول الموظفين ورقم الادارة وموقعها من جدول الادارات
 select emp.empno,emp.ename,emp.deptno,dept.deptno,dept.loc
 from emp,dept
 where emp.deptno=dept.deptno;
